b104426de54f19893c3f34df4ac97b5bc4eadd01
Eduardo Acosta <eduardo.am7049@gmail.com>
2021-02-19 16:34:15 -0600

    Bundler downgrade

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,13 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+Ingredient.create!(name: "lemon")
+Ingredient.create!(name: "ice")
+Ingredient.create!(name: "mint leaves")
+Ingredient.create!(name: "Light rum")
+Ingredient.create!(name: "Applejack")
+Ingredient.create!(name: "Gin")
+Ingredient.create!(name: "Dark rum")
+Ingredient.create!(name: "Sweet Vermouth")
+Ingredient.create!(name: "Strawberry schnapps")
+Ingredient.create!(name: "Scotch")
